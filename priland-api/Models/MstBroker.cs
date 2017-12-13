f9eb5ae3da6e2ae23334091f373c8c7b3d5d6e7c
hgminerva <hgminerva@gmail.com>
2017-12-13 11:55:49 +0800

    Database

---
+++ priland-api/Models/MstBroker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace priland_api.Models
+{
+    public class MstBroker
+    {
+        public Int32 Id {get; set;}
+        public String ProjectCode {get; set;}
+        public String Project {get; set;}
+        public String Address {get; set;}
+        public String Status {get; set;}
+        public Int32 CreatedBy {get; set;}
+        public String CreatedDateTime {get; set;}
+        public Int32 UpdatedBy {get; set;}
+        public String UpdatedDateTime { get; set; }
+    }
+}
