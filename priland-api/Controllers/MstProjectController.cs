1d50e1b78b2413f9291d18f4cacfd72c87fd1059
ArneilPaulPolican <policanarneilpaul@gmail.com>
2017-12-14 10:46:55 +0800

    MstBroker to MstProject Model Add MstProjectController

---
+++ priland-api/Controllers/MstProjectController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Diagnostics;
+using Microsoft.AspNet.Identity;
+using priland_api.Models;
+
+namespace priland_api.Controllers
+{
+    [Authorize]
+    public class MstProjectController : ApiController
+    {
+        private Data.FilbrokerDBDataContext db = new Data.FilbrokerDBDataContext();
+
+        [HttpGet,Route("api/MstProject/List")]
+        public List<Models.MstProject> GetMstProject()
+        {
+            var MstProjectData = from d in db.MstProjects
+                                 select new Models.MstProject
+                                 {
+                                     Id = d.Id,
+                                     ProjectCode = d.ProjectCode,
+                                     Project = d.Project,
+                                     Address = d.Address,
+                                     Status = d.Status,
+                                     CreatedBy = d.CreatedBy,
+                                     CreatedDateTime = d.CreatedDateTime.ToShortDateString() ,
+                                     UpdatedBy = d.UpdatedBy,
+                                     UpdatedDateTime = d.UpdatedDateTime.ToShortDateString()
+                                 };
+            return MstProjectData.ToList();
+        }
+
+        [HttpGet, Route("api/MstProject/Detail")]
+        public Models.MstProject GetMstProjectId(string id)
+        {
+            var MstProjectData = from d in db.MstProjects
+                                 where d.Id==Convert.ToInt32(id)
+                                 select new Models.MstProject
+                                 {
+                                     Id = d.Id,
+                                     ProjectCode = d.ProjectCode,
+                                     Project = d.Project,
+                                     Address = d.Address,
+                                     Status = d.Status,
+                                     CreatedBy = d.CreatedBy,
+                                     CreatedDateTime = d.CreatedDateTime.ToShortDateString(),
+                                     UpdatedBy = d.UpdatedBy,
+                                     UpdatedDateTime = d.UpdatedDateTime.ToShortDateString()
+                                 };
+            return (Models.MstProject)MstProjectData.FirstOrDefault();
+        }
+
+        [HttpPost,Route("api/MstProject/Add")]
+        public int PostMstProject(Models.MstProject addMstProject)
+        {
+            try
+            {
+                Data.MstProject newMstProject = new Data.MstProject();
+
+                newMstProject.ProjectCode = addMstProject.ProjectCode;
+                newMstProject.Project = addMstProject.Project;
+                newMstProject.Address = addMstProject.Address;
+                newMstProject.Status = addMstProject.Status;
+                newMstProject.CreatedBy = addMstProject.CreatedBy;
+                newMstProject.CreatedDateTime = Convert.ToDateTime( addMstProject.CreatedDateTime);
+                newMstProject.UpdatedBy = addMstProject.UpdatedBy;
+                newMstProject.UpdatedDateTime =Convert.ToDateTime( addMstProject.UpdatedDateTime);
+
+                db.MstProjects.InsertOnSubmit(newMstProject);
+                db.SubmitChanges();
+
+                return newMstProject.Id;
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine(e);
+                return 0;
+            }
+        }
+
+        [HttpDelete,Route("api/MstProject/Delete/{id}")]
+        public HttpResponseMessage DeleteMstProject(string id)
+        {
+            try
+            {
+                var MstProjectData = from d in db.MstProjects where d.Id == Convert.ToInt32(id) select d;
+                if (MstProjectData.Any())
+                {
+                    db.MstProjects.DeleteOnSubmit(MstProjectData.First());
+                    db.SubmitChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine(e);
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        [HttpPut,Route("api/MstProject/Update/Detail")]
+        public HttpResponseMessage UpdateProject(string id,Models.MstProject UpdateMstProject)
+        {
+            try
+            {
+                var MstProjectData = from d in db.MstProjects where d.Id == Convert.ToInt32(id) select d;
+                if (MstProjectData.Any())
+                {
+                    var UpdateProjectData = MstProjectData.FirstOrDefault();
+
+                    UpdateProjectData.ProjectCode = UpdateMstProject.ProjectCode;
+                    UpdateProjectData.Project = UpdateMstProject.Project;
+                    UpdateProjectData.Address = UpdateMstProject.Address;
+                    UpdateProjectData.Status = UpdateMstProject.Status;
+                    UpdateProjectData.CreatedBy = UpdateMstProject.CreatedBy;
+                    UpdateProjectData.CreatedDateTime =Convert.ToDateTime( UpdateMstProject.CreatedDateTime);
+                    UpdateProjectData.UpdatedBy = UpdateMstProject.UpdatedBy;
+                    UpdateProjectData.UpdatedDateTime = Convert.ToDateTime( UpdateMstProject.UpdatedDateTime);
+
+                    db.SubmitChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine(e);
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
--- priland-api/Models/MstProject.cs
+++ priland-api/Models/MstProject.cs
@@ -5,7 +5,7 @@ using System.Web;
 
 namespace priland_api.Models
 {
-    public class MstBroker
+    public class MstProject
     {
         public Int32 Id {get; set;}
         public String ProjectCode {get; set;}
@@ -13,8 +13,8 @@ namespace priland_api.Models
         public String Address {get; set;}
         public String Status {get; set;}
         public Int32 CreatedBy {get; set;}
-        public String CreatedDateTime {get; set;}
+        public String  CreatedDateTime {get; set;}
         public Int32 UpdatedBy {get; set;}
-        public String UpdatedDateTime { get; set; }
+        public String  UpdatedDateTime { get; set; }
     }
 }
