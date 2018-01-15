88349c95d06241d567a0f04cf9095e081db88658
ArneilPaulPolican <policanarneilpaul@gmail.com>
2018-01-15 13:43:31 +0800

    PDF Template

--- priland-api/Controllers/RepPdfController.cs
+++ priland-api/Controllers/RepPdfController.cs
@@ -1,21 +1,46 @@
-﻿using System;
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace priland_api.Controllers
 {
-    [Authorize]
-    [RoutePrefix("api/pdf")] 
     public class RepPdfController : Controller
     {
+        // ================
+        // Global Variables
+        // ================
+        public MemoryStream workStream = new MemoryStream();
+        public Rectangle rectangle = new Rectangle(PageSize.A3);
 
         // GET: pdf/customer/5
-        [HttpGet, Route("Customer/{id}")]
         public ActionResult PdfCustomer(int id)
         {
-            return View();
+            // ==============================
+            // PDF Settings and Customization
+            // ==============================
+            Document document = new Document(rectangle, 72, 72, 72, 72);
+            document.SetMargins(30f, 30f, 30f, 30f);
+            PdfWriter.GetInstance(document, workStream).CloseStream = false;
+
+            document.Open();
+
+            Paragraph line = new Paragraph(new Chunk(new iTextSharp.text.pdf.draw.LineSeparator(0.0F, 100.0F, BaseColor.BLACK, Element.ALIGN_LEFT, 4.5F)));
+
+            document.Add(line);
+
+
+            document.Close();
+
+            byte[] byteInfo = workStream.ToArray();
+            workStream.Write(byteInfo, 0, byteInfo.Length);
+            workStream.Position = 0;
+
+            return new FileStreamResult(workStream, "application/pdf");
         }
 
         // GET: pdf/broker/5
